ae8f8350674fd38ee56ade4111601bdcaf3cfacf
FarhanAttique <Farhanatique44@gmail.com>
2024-10-28 14:43:41 +0000

    first commit

---
+++ payment_reconciliation/payment_reconciliation/doctype/record/record.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Farhan and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Record', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ payment_reconciliation/payment_reconciliation/doctype/record/test_record.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Farhan and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestRecord(FrappeTestCase):
+	pass
